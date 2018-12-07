3ad55d00ea9b34250117a81f416edddfa3f55be5
Nikolas De Giorgis <nikolas.degiorgis@iit.it>
2018-12-07 15:12:40 +0100

    templated multivector initialization

--- intent.h
+++ intent.h
@@ -1,21 +1,50 @@
 #include <vector>
 #include <iostream>
 #include <tuple>
+#include <limits>
 namespace intent
 {
   namespace utility
   {
-    template<size_t dimcount, typename T>
+
+    class default_t
+    {
+    public:
+      template<typename T>
+        operator T() const {return T();}
+    };
+    default_t const defaultVal=default_t();
+
+    template <size_t dimcount, typename T>
       struct multidimensional_vector
       {
         typedef std::vector< typename multidimensional_vector<dimcount-1, T>::type > type;
+
       };
 
     template<typename T>
-      struct multidimensional_vector<0,T>
+      struct multidimensional_vector<0,T> 
       {
         typedef T type;
       };
+
+    template <size_t dim, typename T, typename V>
+    void defInit(int size, T val, std::vector<T>& v)
+    {
+      v=std::vector<T>(size,val);
+    }
+
+    template <size_t dim, typename T, typename V>
+      void defInit(int size, T val, V& v)
+    {
+       v=std::vector<typename intent::utility::multidimensional_vector<(dim - 1), T>::type>(size);
+       for (int i=0; i< size; ++i)
+         defInit<dim-1,T,decltype(v[i])>(size,val,v[i]);
+    }
+
+ 
+
+
   }
 
 
@@ -26,6 +55,15 @@ namespace intent
 
     using bbox = std::tuple<int,int,int,int>;
   public:
+
+    Grid()
+    {
+      _corners=bbox(0,0,std::numeric_limits<int>::max(),std::numeric_limits<int>::max());
+    }
+    Grid(const utility::default_t &t)
+    {
+      _corners=bbox(0,0,std::numeric_limits<int>::max(),std::numeric_limits<int>::max());
+    }
     Grid(int minX, int minY, int maxX, int maxY)
       {
         _corners=bbox(minX,minY,maxX,maxY);
--- manualTesting.cpp
+++ manualTesting.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main()
 {
   intent::Grid g(0,0,5,5);
-  g.printDebugInfo();
+  intent::utility::multidimensional_vector<5,int>::type v;
+
+  intent::utility::defInit<5,int, decltype(v)>(10,2,v);
+  
+  cout << v[0][3][4][3][1] << endl;
 }
 
